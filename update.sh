efdd707f2f97e8456362d44206d8aa521558d96d
Trevor <56496067+Hifumi-Sec@users.noreply.github.com>
2021-09-07 04:16:54 -0500

    simple update script for new versions

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+############################################################
+# Universal CTF | v1.0.0                                   #
+# Creator: Hifumi Sec (https://github.com/Hifumi-Sec)      #
+# Open-Source: https://github.com/Hifumi-Sec/universal-ctf #
+############################################################
+
+cd ..
+
+sudo rm -r universal-ctf
+
+git clone https://github.com/Hifumi-Sec/universal-ctf.git
+
+echo "Newest version of Universal CTF is now available!"
