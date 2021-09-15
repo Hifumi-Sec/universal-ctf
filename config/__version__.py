7ddbc63ba912003f000c2fec69470d62f32f38f5
Trevor <56496067+Hifumi-Sec@users.noreply.github.com>
2021-09-15 03:11:02 -0500

    v1.0.6

--- config/__version__.py
+++ config/__version__.py
@@ -1,7 +1,7 @@
 ############################################################
-# Universal CTF | v1.0.4                                   #
+# Universal CTF | v1.0.6                                   #
 # Creator: Hifumi Sec (https://github.com/Hifumi-Sec)      #
 # Open-Source: https://github.com/Hifumi-Sec/universal-ctf #
 ############################################################
 
-VERSION = 'v1.0.4'
+VERSION = 'v1.0.6'
